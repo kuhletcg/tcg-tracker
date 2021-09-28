1aa9f37023bd911bf7cbe259a892f336aaae0b87
TCG <ssibonokuhle@gmail.com>
2021-09-28 09:55:37 +0200

    Added route in app.js file

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Nav from "./components/Navbar";
+import HomePage from "./components/HomePage";
+import Tweet from "./components/Tweet";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Nav />
+        <Switch>
+          <Route path="/" exact component={HomePage} />
+          <Route path="/tweets" exact component={Tweet} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
